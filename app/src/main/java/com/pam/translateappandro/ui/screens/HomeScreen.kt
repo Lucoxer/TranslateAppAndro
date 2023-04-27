89031be9dff746ae2f1c8870541266f584c4d268
Ambrosius <ambroius.5ludang@gmail.com>
2023-04-27 14:19:06 +0700

    Homescreen design roughness

--- app/src/main/java/com/pam/translateappandro/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/pam/translateappandro/ui/screens/HomeScreen.kt
@@ -1,15 +1,20 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package com.pam.translateappandro.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -18,19 +23,24 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.pam.translateappandro.R
 import com.pam.translateappandro.ViewModel.sharedViewModel
+
 
 @Composable
 fun HomeScreen(
     navController: NavHostController = rememberNavController(),
-    sharedViewModel: sharedViewModel
+    sharedViewModel: sharedViewModel,
 ){
-
+    var expanded by remember { mutableStateOf(false) }
+    var toTrans by remember { mutableStateOf("") }
+    var trans by remember { mutableStateOf("") }
     var name by rememberSaveable{ mutableStateOf("") }
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
     val setUserInformation = sharedViewModel.person
     val user = Firebase.auth.currentUser!!
+
 
     sharedViewModel.retrieveData(
         email = setUserInformation!!.email,
@@ -56,14 +66,31 @@ fun HomeScreen(
             fontWeight = FontWeight.Bold
         )
     }
-    Spacer(modifier = Modifier.height(16.dp))
-    Card(modifier = Modifier) {
-        Row(
-            modifier = Modifier
-                .padding(20.dp)
-                .background(Color.LightGray)
-        ) {
 
+
+    Column(modifier = Modifier.padding(20.dp)) {
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(text = "English")
+        TextField(
+            value = toTrans.toString(),
+            onValueChange = {toTrans = it},
+            label = {Text(text = "Input Text to be Translate")},
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Translate")
         }
+        Spacer(modifier = Modifier.height(70.dp))
+        Text(text = "Indonesia")
+        TextField(
+            value = trans.toString(),
+            onValueChange = {trans = it},
+            label = {Text(text = "Your Translate")},
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+        )
     }
 }
